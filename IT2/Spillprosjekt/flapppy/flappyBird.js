a66b1ab4da95610337ce3c797bddaf29fc5cf0aa
herm2607 <42634720+herm2607@users.noreply.github.com>
2019-03-28 22:16:22 +0100

    Test

--- IT2/Spillprosjekt/flapppy/flappyBird.js
+++ IT2/Spillprosjekt/flapppy/flappyBird.js
@@ -1,7 +1,7 @@
 var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+var ctx = cvs.getContext("2d")
 
-// alle bilder
+// bildene som brukes i spillet
 
 var bird = new Image();
 var bg = new Image();
@@ -9,38 +9,44 @@ var fg = new Image();
 var pipeNorth = new Image();
 var pipeSouth = new Image();
 
-bird.src = "sad.png";
+bird.src = "bird.png";
 bg.src = "bg.png";
 fg.src = "fg.png";
 pipeNorth.src = "pipeNorth.png";
 pipeSouth.src = "pipeSouth.png";
 
 
-// variabler
+// Variabler som fastsetter lengde, gap, gravity osv. på pipene og fugl
 
-var gap = 85;
+var gap = 110;
 var constant;
 
 var bX = 10;
 var bY = 150;
 
-var gravity = 1.3;
+var gravity = 1.5;
 
 var score = 0;
 
-// lyd
+// Dette er lydfilene som skal spilles av når en tast trykkes.
+// I tilleg til scorelyd når fuglen har passert enten pipeNorth eller pipeSouth
 
 var fly = new Audio();
 var scor = new Audio();
 
 fly.src = "fly.mp3";
-scor.src = "score.mp3";
+scor.src = "oof1.mp3";
 
-// knapp
+// Når tast er trykket ned beveger fuglen seg oppover
 
+document.addEventListener("keydown",moveUp);
 
+function moveUp(){
+    bY -= 37;
+    fly.play();
+}
 
-// pipeposisjoner
+// pipens koordinater
 
 var pipe = [];
 
@@ -49,7 +55,7 @@ pipe[0] = {
     y : 0
 };
 
-// tegn bilder
+// Her tegnes bildene. Feks X og Y posisjon og bredde og høyde
 
 function draw(){
 
@@ -57,7 +63,7 @@ function draw(){
 
 
     for(var i = 0; i < pipe.length; i++){
-
+        // Her tegner jeg pipeNorth og pipeSouth
         constant = pipeNorth.height+gap;
         ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
         ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
@@ -71,23 +77,31 @@ function draw(){
             });
         }
 
-        // kræsj
+        // Her oppdages kollisjonen hvis fuglen treffer enten pipeNorth eller pipeSouth
 
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+            location.reload(); // reloader siden
+        }
 
+        if(pipe[i].x == 5){
+            score++;
+            scor.play();
+        }
 
 
     }
 
     ctx.drawImage(fg,0,cvs.height - fg.height);
-
+// her tegner jeg fuglen
     ctx.drawImage(bird,bX,bY);
 
     bY += gravity;
 
-    ctx.fillStyle = "#000";
-    ctx.font = "20px Arial";
+    ctx.fillStyle = "white";
+    ctx.font = "40px Verdana";
     ctx.fillText("Score : "+score,10,cvs.height-20);
 
+    // bruker denne funksjonen for å tegne ovennevnte på nytt konstant
     requestAnimationFrame(draw);
 
 }
